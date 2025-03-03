d9ef0ea6f3ebd55a85381830a92a15100ca39ab1
treehog2 <treehog222@gmail.com>
2025-03-03 15:42:35 -0500

    v0.1.0

--- README.md
+++ README.md
@@ -1,19 +1,37 @@
-# Development Setup
+#  IINA AB Save Plugin
+This plugin allows saving active AB loops as individual files.
 
-## IINA
+Note: this plugin is currently only usable in development mode. Not directly importable via github as of yet.
 
-0. `defaults write com.colliderli.iina iinaEnablePluginSystem -bool YES` enables plugins under IINA -> Settings. [Source](https://github.com/iina/iina/releases/tag/v1.3.4)
-1. `ln -s /Applications/IINA.app/Contents/MacOS/iina-plugin /usr/local/bin/iina-plugin`
-2. `iina-plugin pack .`
-3. ...
-4. `iina-plugin link .` (for local development, keeps the plugin installed)
+# How to Use
+
+## Requirements
+- `ffmpeg` (tested with version `6.0-tessus`).
+- `iina` (tested with version `1.3.5` Build 141). [Download Exact Nightly Build](https://github.com/iina/iina/actions/runs/13537465959/artifacts/2653783517).
+
+## Setup
+
+1. Run `defaults write com.colliderli.iina iinaEnablePluginSystem -bool YES` which enables plugins under IINA -> Settings. [Source](https://github.com/iina/iina/releases/tag/v1.3.4)
+2. Run `ln -s /Applications/IINA.app/Contents/MacOS/iina-plugin /usr/local/bin/iina-plugin` to use the executable `iina-plugin`
+3. Run `git clone https://github.com/treehog2/iina-absave-plugin` and `cd iina-absave-plugin`
+4. Run `iina-plugin link .` (for local development, keeps the plugin installed). You can later run `iina-plugin unlink .` to uninstall.
+5. Re/start IINA -> Settings -> Plugins -> Save AB Loop -> Preferences -> Set `FFmpeg Path` to whatever `where ffmpeg` in your terminal returns.
+
+## Plugin Settings
+
+- `ffmpeg_path`: path for the ffmpeg executable.
+- `save_dir`: change where loop files are saved. defaults to the same directory as the video file if left blank.
+- `max_file_name_length`: if set, will truncate the output file name to this value.
 
 
+# Development Tips
+- View [Getting Started](https://docs.iina.io/pages/getting-started.html)
+- View [API docs](https://docs.iina.io/modules/IINA.API.html)
+- Once the plugin is running, you can use the top bar MacOS menu -> Plugins -> Developer Tool -> <Plug In Name> to access the console for the plugins locally accessible variables.
+- For the latest builds which may have necessary bug fixes, view IINA Nightly builds [here](https://iina.io/nightly/). [Source](https://github.com/iina/iina/tree/develop)
 
-## Local Git Development
 
-```plaintext
-git config user.name <name>
-git config user.email <email>
-git remote set-url origin https://<username>:PAT@github.com/treehog2/iina-absave-plugin.git
-```
+# TODO
+- [x] Send confirmation messages to user in case of errors or success.
+- [ ] Support importing plugin directly from github.
+- [ ] Figure out why `ffmpeg` executable is not accessible to the plugin.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,125 @@
-const {
-  console,
-  menu,
-} = iina;
+const { menu, mpv, utils, core, console, file, input, preferences } = iina;
 
-console.log("Plugin is running");
+// Function to convert file URL to a standard path
+function fileURLToPath(url) {
+    return decodeURIComponent(url.replace('file://', ''));
+  }
+
+function logAndAlert(msg, isError = false) {
+  if (isError) {
+    console.error(msg);
+  } else {
+    console.log(msg);
+  }
+  utils.ask(msg);
+}
 
 
+async function createOutputPath(inputPath, start, end) {
+  // Split the path into components
+  const parts = inputPath.split('/');
+  const fileName = parts[parts.length - 1];
+  start = Math.round(start, 2);
+  end = Math.round(end, 2);
+
+  // Get the directory
+  const directory = preferences.get("save_dir") || parts.slice(0, -1).join('/');
+  console.log(`directory: ${directory}`);
+  // Split the filename into name and extension
+  const lastDotIndex = fileName.lastIndexOf('.');
+  const name = lastDotIndex !== -1 ? fileName.slice(0, lastDotIndex) : fileName;
+  const extension = lastDotIndex !== -1 ? fileName.slice(lastDotIndex) : '';
+
+  // Create the new filename
+  const maxNameLength = preferences.get("max_file_name_length");
+  const truncated_name = maxNameLength && maxNameLength > 0 && name.length > maxNameLength
+    ? name.substring(0, Math.floor(maxNameLength / 2) - 1) + '..' + name.substring(name.length - Math.floor(maxNameLength / 2) + 1)
+    : name;
+  const newFileName = `${truncated_name}_${start}_${end}${extension}`;
+
+  // Combine everything
+  return `${directory}/${newFileName}`;
+}
+
+
+// Define the action to save the A-B loop
+async function saveABLoop() {
+  const ffmpegPath = preferences.get("ffmpeg_path");
+  // Get the current A and B loop points
+  const loopStart = mpv.getNumber('ab-loop-a');
+  const loopEnd = mpv.getNumber('ab-loop-b');
+  console.log(`core.status.url: ${core.status.url}`);
+  console.log(`A-B loop points: ${loopStart} - ${loopEnd}`);
+
+  if (!utils.fileInPath(ffmpegPath)) {
+    const msg = `ffmpeg not found at path: ${ffmpegPath}`;
+    logAndAlert(msg, true);
+    return;
+  }
+
+
+  // Check if both loop points are set
+  if (loopStart === 0 || loopEnd === 0) {
+    const msg = 'Both A-B loop points are not properly set.';
+    logAndAlert(msg, true);
+    return;
+  }
+
+  const fileURL = core.status.url;
+  if (!fileURL) {
+    const msg = 'No file is currently playing.';
+    logAndAlert(msg, true);
+    return;
+  }
+
+  const inputPath = fileURLToPath(fileURL);
+
+  // Define the output file path
+  const outputPath = await createOutputPath(inputPath, loopStart, loopEnd);
+  console.log(`Output path: ${outputPath}`);
+
+  const ffmpegArgs = [
+    '-i', inputPath,
+    '-ss', String(Math.round(loopStart, 2)),
+    '-to', String(Math.round(loopEnd, 2)),
+    '-c', 'copy',
+    outputPath,
+    '-y'
+  ];
+
+  console.log(`ffmpegCommand: ${ffmpegPath} ${ffmpegArgs.join(' ')}`);
+
+  // Execute the ffmpeg command
+  // if debugging ffmpeg, you can run this instead: const { status, stdout, stderr } = await utils.exec(ffmpegPath, ["-i", inputPath]);
+
+  try {
+    const { status, stdout, stderr } = await utils.exec(ffmpegPath, ffmpegArgs);
+    if (status === 0) {
+      const msg = `A-B loop saved successfully to ${outputPath}`;
+      logAndAlert(msg, false);
+      // Does not work for some reason but good intention
+      // Source: https://docs.iina.io/interfaces/IINA.API.File.html#revealInFinder
+      // file.revealInFinder(outputPath);
+    } else {
+      const msg = `ffmpeg failed with status=${status}. err=${stderr}. out=${stdout}`;
+      logAndAlert(msg, true);
+    }
+  } catch (error) {
+    const msg = `Error executing ffmpeg: ${error}`;
+    logAndAlert(msg, true);
+  }
+}
+
+// Create the menu item
+const saveLoopMenuItem = menu.item('Save A-B Loop', saveABLoop);
+
+// Add the menu item to IINA's Plugin menu
+menu.addItem(saveLoopMenuItem);
+
+
+// https://docs.iina.io/interfaces/IINA.API.Input.html
+input.onKeyDown('Alt+k', async () => {
+  console.log('Alt+k key pressed');
+  saveABLoop();
+  return true;
+});
